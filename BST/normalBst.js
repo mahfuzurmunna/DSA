dd808cfdaf3adf9d5bdd9b73c42bab1bb9500b29
Mahfuzur Rahman Munna <mahfuzurrm07@gmail.com>
2024-06-13 21:09:21 +0600

    bst insertion

---
+++ BST/normalBst.js
@@ -0,0 +1,39 @@
+//basic binary search implementation
+
+class BSTNode {
+  constructor(key) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(key) {
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.key < node.key) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+}
