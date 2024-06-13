8ced6b8dadba7fbb2c207983e45788bb0c0441e1
Mahfuzur Rahman Munna <mahfuzurrm07@gmail.com>
2024-06-13 21:35:35 +0600

    deletion

--- BST/normalBst.js
+++ BST/normalBst.js
@@ -36,4 +36,40 @@ class BinarySearchTree {
       }
     }
   }
+
+  delete(key) {
+    this.root = this.deleteNode(this.root, key);
+  }
+
+  deleteNode(node, key) {
+    // node -> root
+    if (node === null) {
+      return null;
+    }
+
+    if (key < node.key) {
+      node.left = this.deleteNode(node.left, key);
+    } else if (key > node.key) {
+      node.right = this.deleteNode(node.right, key);
+    } else {
+      if (node.left === null && node.right === null) {
+        return null;
+      } else if (node.left === null) {
+        return node.right;
+      } else if (node.right === null) {
+        return node.left;
+      } else {
+        let tempNode = this.findMinNode(node);
+        node.key = tempNode.key;
+        node.right = this.deleteNode(node, tempNode.key);
+      }
+    }
+  }
+
+  findMinNode(node) {
+    while (node.left !== null) {
+      node = node.left;
+    }
+    return node;
+  }
 }
