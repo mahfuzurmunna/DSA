4272179e65f8ba795825bb1263bc3ad18f9e71ed
Mahfuzur Rahman Munna <mahfuzurrm07@gmail.com>
2024-07-03 12:38:13 +0600

    directed path

--- Graph/problems/directed_path.js
+++ Graph/problems/directed_path.js
@@ -1,3 +1,21 @@
+/*
+Given two nodes (start and dest) in a directed acyclic graph (DAG), return true if there is a directed path from start to dest, otherwise return false.
+
+Example 1:
+
+Input: start = 1, dest = 3
+Output: true
+Explanation: There is a directed path 1 -> 2 -> 3.
+Example 2:
+
+Input: start = 1, dest = 4
+Output: false
+Explanation: There is no path from 1 to 4.
+
+
+
+*/
+
 function hasPathDFS(graph, start, dest) {
   const stack = [start];
   const visited = new Set();
@@ -9,13 +27,11 @@ function hasPathDFS(graph, start, dest) {
     }
     if (!visited.has(node)) {
       visited.add(node);
-
       for (const neighbour of graph[node]) {
         stack.push(neighbour);
       }
     }
   }
-
   return false;
 }
 
@@ -26,7 +42,6 @@ function hasPathBFS(graph, start, dest) {
   while (queue.length > 0) {
     const node = queue.shift();
     if (node === dest) return true;
-
     if (!visited.has(node)) {
       visited.add(node);
       for (const neighbour of graph[node]) {
@@ -37,14 +52,21 @@ function hasPathBFS(graph, start, dest) {
   return false;
 }
 
-const hasPathRecursive = (graph, start, dest) => {
+function hasPathRec(graph, start, dest, visited = new Set()) {
   if (start === dest) return true;
 
+  //just 4 lines for visited
+  if (visited.has(start)) {
+    return false;
+  }
+  visited.add(start);
+
+  //rest all is good
+
   for (const neighbour of graph[start]) {
-    if (hasPathRecursive(graph, neighbour, dest) === true) {
+    if (hasPathRec(graph, neighbour, dest) === true) {
       return true;
     }
   }
-
   return false;
-};
+}
---
+++ Graph/problems/undirectedPath.js
@@ -0,0 +1,22 @@
+/*
+const edges = [
+  ['b', 'a'],
+  ['c', 'a'],
+  ['b', 'c'],
+  ['q', 'r'],
+  ['q', 's'],
+  ['q', 'u'],
+  ['q', 't'],
+];
+const graph = {
+  b: [a],
+  a: [b]
+}
+loop => check if DOES NOT exist, then add key => push both nodes
+
+Write a function, undirectedPath, that takes in an array of edges for an undirected graph and two nodes.
+
+After that you can find and node path etc.
+Take care of cycle via visited technique.
+
+*/
