bfe184b3f1b39f8a8d12140764b01038f773d6d4
Mahfuzur Rahman Munna <mahfuzurrm07@gmail.com>
2024-07-11 23:52:29 +0600

    compress start

---
+++ Problems/Leetcode/10_compress_algo.js
@@ -0,0 +1,22 @@
+var compress = function (chars) {
+  let index = 0;
+  let i = 0;
+
+  while (i < chars.length) {
+    let j = i;
+
+    while (j < chars.length && chars[j] === chars[i]) {
+      j++;
+    }
+
+    let count = j - 1;
+    chars[index++] = chars[i];
+
+    if (count > 1) {
+      for (const str of count.toString()) {
+        chars[index++] = str;
+      }
+    }
+    return index;
+  }
+};
