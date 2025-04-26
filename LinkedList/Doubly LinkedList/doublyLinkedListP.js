c16141ef3b9cbcfff0182bb4393ef6e55e6e99cd
Mahfuzur Rahman Munna <mahfuzurrm07@gmail.com>
2025-04-26 21:41:04 +0600

    dll again

--- LinkedList/Doubly LinkedList/doublyLinkedListP.js
+++ LinkedList/Doubly LinkedList/doublyLinkedListP.js
@@ -1,49 +1,68 @@
-//node class
+//doubly linked list implementation using javascript class
+
+// node creation
 class Node {
-  constructor(data, left, right) {
+  constructor(data, next = null, prev = null) {
     this.data = data;
-    this.left = left;
-    this.right = right;
+    this.next = next;
+    this.prev = prev;
   }
 }
 
-// doubly linked list constructor
+// doubly linkedlist implementation
 class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
   }
-}
 
-// insert at first method
-DoublyLinkedList.prototype.insertAtfirst = function (data) {
-  const newNode = new Node(data, null, this.head);
-  if (this.head) {
-    this.head.left = newNode;
-  }
-
-  this.head = newNode;
-
-  if (!this.tail) {
-    this.tail = newNode;
-  }
-};
-
-//insert at last method
-DoublyLinkedList.prototype.insertAtLast = function (data) {
-  const newNode = new Node(data, this.tail, null);
-
-  if (this.tail) {
-    this.tail.left = newNode;
-  }
-  this.tail = newNode;
-  if (!this.head) {
+  // insert at first data
+  insertAtFirst(data) {
+    const newNode = new Node(data, null, this.head);
+    if (this.head) {
+      this.head.next = newNode;
+    }
     this.head = newNode;
+    if (!this.tail) {
+      this.tail = newNode;
+    }
   }
-};
 
-//insert after a given node/key
-DoublyLinkedList.prototype.insertAfterGivenNode = function (prevNode,data) {
-  
+  //insert at last
+  insertAtLast(data) {
+    const newNode = new Node(data, this.tail, null);
+    if (this.tail) {
+      this.tail.right = newNode;
+    }
+    this.tail = newNode;
+    if (!this.head) {
+      this.head = newNode;
+    }
+  }
+
+  //insert after a given node
+  insertAfterGivenNode(prevNode, data) {
+    const newNode = new Node(data, prevNode.next, prevNode);
+
+    // if the linkedList is empty
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+
+    if (!prevNode) {
+      console.log("PrevNode not found, please enter a valid node");
+      return;
+    }
+    if (prevNode.next) {
+      prevNode.next.prev = newNode;
+    }
+    prevNode.next = newNode;
+    if (!prevNode.next) {
+      this.tail = newNode;
+    }
+  }
+
+  //
 }
- 
