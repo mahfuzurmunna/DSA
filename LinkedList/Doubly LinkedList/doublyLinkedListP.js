4f4a5f80a475951f46f86ab3f951484c29d207c8
Mahfuzur Rahman Munna <mahfuzurrm07@gmail.com>
2025-04-27 23:39:06 +0600

    just pushing :)

--- LinkedList/Doubly LinkedList/doublyLinkedListP.js
+++ LinkedList/Doubly LinkedList/doublyLinkedListP.js
@@ -18,9 +18,9 @@ class DoublyLinkedList {
 
   // insert at first data
   insertAtFirst(data) {
-    const newNode = new Node(data, null, this.head);
+    const newNode = new Node(data, this.head, null);
     if (this.head) {
-      this.head.next = newNode;
+      this.head.prev = newNode;
     }
     this.head = newNode;
     if (!this.tail) {
@@ -30,9 +30,9 @@ class DoublyLinkedList {
 
   //insert at last
   insertAtLast(data) {
-    const newNode = new Node(data, this.tail, null);
+    const newNode = new Node(data, null, this.tail);
     if (this.tail) {
-      this.tail.right = newNode;
+      this.tail.next = newNode;
     }
     this.tail = newNode;
     if (!this.head) {
@@ -64,5 +64,79 @@ class DoublyLinkedList {
     }
   }
 
-  //
+  //delete first node
+  delelteFirst() {
+    //if the list is empty
+    if (!this.head) {
+      console.log("List is empty");
+      return;
+    }
+
+    // if the list has only one node
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+      return;
+    }
+
+    // deleting the current head and making the head.next as a head;
+    this.head = this.head.next;
+
+    this.head.prev = null;
+  }
+
+  deleteLast() {
+    if (!this.head) {
+      console.log("List is empty");
+      return;
+    }
+
+    // if the list has only one node
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+      return;
+    }
+
+    this.tail = this.tail.prev;
+    this.tail.next = null;
+  }
+
+  reverse() {
+    let currentNode = this.head;
+    let temp = null;
+
+    while (currentNode) {
+      temp = currentNode.prev;
+      currentNode.prev = currentNode.next;
+      currentNode.next = temp;
+
+      currentNode = currentNode.next;
+    }
+    if (temp) {
+      this.tail = this.head;
+      this.head = temp.prev;
+    }
+  }
+
+  printList() {
+    let list = [];
+    let currentNode = this.head;
+    while (currentNode) {
+      list.push(currentNode.data);
+      currentNode = currentNode.next;
+    }
+    return list.join(" -> ");
+  }
 }
+
+const newDLL = new DoublyLinkedList();
+newDLL.insertAtFirst(10);
+newDLL.insertAtFirst(20);
+newDLL.insertAtFirst(30);
+newDLL.insertAtFirst(40);
+newDLL.insertAtLast(0);
+newDLL.delelteFirst();
+newDLL.deleteLast();
+newDLL.reverse();
+console.log(newDLL.printList());
